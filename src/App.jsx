4712c5ac90447f76f93b34471a82bb48a04fb73c
ButSeavThong <rope61963@gmail.com>
2025-10-10 23:18:30 +0700

    added analytic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import HeroSection from './components/layouts/hero-section'
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
 
   return (
     <>
     <HeroSection/>
+    <Analytics />
     </>
   ) 
 }
